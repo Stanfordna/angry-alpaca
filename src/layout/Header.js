4d7aee04eec87759c23e474abcb4403eb82447e1
Nicholas Stanford <stanfordnas@gmail.com>
2023-09-20 15:30:59 -0600

    Added some dependencies, working on contact envelope button

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,7 @@
 import './Header.css';
+import { Button } from '@react-email/button';
+import { Html } from '@react-email/html';
+import envelope from '../images/envelope.png';
 
 function Header() {
     return (
@@ -10,6 +13,11 @@ function Header() {
         <header-contact>
           CONTACT
         </header-contact>
+        <header-envelope 
+          style={{ backgroundImage: `url(${envelope})` }} >
+          <Button
+            href="https://google.com" />
+        </header-envelope>
       </site-header>
     );
 }
