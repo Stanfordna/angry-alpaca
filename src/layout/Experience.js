3c7838876a64e945f676684796e4ad84203f5413
Nicholas Stanford <stanfordnas@gmail.com>
2023-09-25 20:46:32 -0600

    added SSL cert instructions to start script. Continued development of CSS and Experience Component content.

--- src/layout/Experience.js
+++ src/layout/Experience.js
@@ -16,13 +16,16 @@ function Experience() {
           </my-about>
           <my-positions>
             <h2>Experience</h2>
-            <position-duration>
-              September 2022 to Present
-            </position-duration>
             <position-details>
-              <h3>Web Application Developer</h3>
-              Contracting with Sandia National Labs in Albuquerque, New Mexico to build 
-              full stack applications and automate workflows for the Department of Energy.
+              <position-title>Web Application Developer</position-title>
+              <position-spacer />
+              <position-duration>
+                September 2022 to Present
+              </position-duration>
+              <position-description>
+                Contracting with Sandia National Labs in Albuquerque, New Mexico to build 
+                full stack applications and automate workflows for the Department of Energy.
+              </position-description>
               <position-skills>
                 <my-skill>Apache Tomcat</my-skill>
                 <my-skill>SQL</my-skill>
@@ -32,14 +35,17 @@ function Experience() {
                 <my-skill>CSS</my-skill>
               </position-skills>    
             </position-details>
-            <position-duration>
-              July 2020 to September 2022
-            </position-duration>
             <position-details>
-              <h3>Systems Analyst • Developer</h3>
-              Migrated UNM web applications onto Drupal CMS. Built custom modules for Drupal using PHP and Javascript.
-              Used shell and Python scripting to retrieve, process and report project milestone progress data to 
-              the National Institutes of Health.
+              <position-title>Systems Analyst • Developer</position-title>
+              <position-spacer />
+              <position-duration>
+                July 2020 to September 2022
+              </position-duration>
+              <position-description>
+                Migrated UNM web applications onto Drupal CMS. Built custom modules for Drupal using PHP and Javascript.
+                Used shell and Python scripting to retrieve, process and report project milestone progress data to 
+                the National Institutes of Health.
+              </position-description>
               <position-skills>
                 <my-skill>Apache HTTP Server</my-skill>
                 <my-skill>PHP</my-skill>
@@ -51,14 +57,17 @@ function Experience() {
                 <my-skill>Bash</my-skill>
               </position-skills>    
             </position-details>
-            <position-duration>
-              October 2018 to July 2020
-            </position-duration>
             <position-details>
-              <h3>Webserver Support Technician • Wordpress Developer</h3>
-              Assisted customers with deployment of sites and implementation of custom Wordpress modules.
-              Managed web hosting, domain name email, and name servers. Monitored mail queues for suspicious traffic 
-              and responded to cybersecurity threats against web and mail servers urgently and efficiently. 
+              <position-title>Webserver Support Technician • Wordpress Developer</position-title>
+              <position-spacer />
+              <position-duration>
+                October 2018 to July 2020
+              </position-duration>
+              <position-description>
+                Assisted customers with deployment of sites and implementation of custom Wordpress modules.
+                Managed web hosting, domain name email, and name servers. Monitored mail queues for suspicious traffic 
+                and responded to cybersecurity threats against web and mail servers urgently and efficiently. 
+              </position-description>
               <position-skills>
                 <my-skill>Apache HTTP Server</my-skill>
                 <my-skill>DNS</my-skill>
@@ -74,52 +83,50 @@ function Experience() {
           <my-projects>
             <h2>Personal Projects</h2>
             <purpose-statement>I feel stuff when I do stuff</purpose-statement>
-            <personal-projects>
-              <project-details>
-                <h3>NicholasStanford.dev</h3>
+            <project-details>
+              <project-title>NicholasStanford.dev</project-title>
+              <project-description>
                 I made this website
-                <project-feature>
-                  It has an SMTP server
-                </project-feature>
-                <project-skills>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                </project-skills>
-                <project-feature>
-                  The background draws from the APOD API
-                </project-feature>
-                <project-skills>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                </project-skills>
-              </project-details>
-            </personal-projects>
+              </project-description>
+              <project-feature>
+                It has an SMTP server
+              </project-feature>
+              <project-skills>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+              </project-skills>
+              <project-feature>
+                The background draws from the APOD API
+              </project-feature>
+              <project-skills>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+              </project-skills>
+            </project-details>
           </my-projects>
           <my-education>
             <h2>Education</h2>
             <my-degree>B.S. Computer Science • University of Colorado</my-degree>
-            <school-projects>
-              <project-details>
-                <h3>Senior Project</h3>
-                I did an thing with Chunity
-                <project-skills>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                </project-skills>
-              </project-details>
-              <project-details>
-                <h3>Something on Github</h3>
-                I did a gif app for Android
-                <project-skills>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                  <my-skill>sKill</my-skill>
-                </project-skills>
-              </project-details>
-            </school-projects>
+            <project-details>
+              <project-title>Senior Project</project-title>
+              I did an thing with Chunity
+              <project-skills>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+              </project-skills>
+            </project-details>
+            <project-details>
+              <project-title>Something on Github</project-title>
+              I did a gif app for Android
+              <project-skills>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+                <my-skill>sKill</my-skill>
+              </project-skills>
+            </project-details>
           </my-education>
         </experience-body>
       </experience-container>
