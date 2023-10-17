5fe205c3099022be0a584405f54f7caa89205746
Nicholas Stanford <stanfordnas@gmail.com>
2023-10-16 18:37:23 -0600

    Minor changes to site, added ssl configuration to dockerfile instead of setting environment variables in npm "run start-ssl" script. Add named volume for ssl to docker compose file.

--- src/backgrounds/APOD.js
+++ src/backgrounds/APOD.js
@@ -4,7 +4,7 @@ import apod from './assets/apod.jpg'
 function APOD() {
     return (
       <apod-container>
-        <img src={apod} id="img-apod"/>
+        <img src={apod} id="img-apod" alt="NASA Astronomy pic of the Day"/>
       </apod-container>
     );
 }
