7c83340b674df85170b9e259e02292231965fecd
Nicholas <stanfordnas@gmail.com>
2025-01-15 16:30:57 -0700

    20250115
    Made docker_run.sh pull and retag angry-alpaca with a nice name.

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
+# using "version" here to mean "tag"
 version='0.1.0'
 name='angry-alpaca'
+docker_hub_url='docker.io/angryalpaca'
 volume_opts='-v /ssh:/home/stanf/.ssh'
 
-echo "Running Container: $name "
-docker run --rm \
-    --name $name \
-    $volume_opts \
-    $name:$version
-
-if ask "Shell into container?"
+if [ -z "`docker images -q $name`" ] && ask "No local container $name found. Pull image $docker_hub_url/$name:$version?"
 then
-    docker exec -it $name bash
+    docker pull $docker_hub_url/$name:$version $name:$version
+    echo "Pulled $docker_hub_url/$name:$version as $name:$version"
+fi
+
+if ! [ -z "`docker images -q $name`" ]
+    then
+    echo "Running Container: $name "
+    docker run --rm \
+        --name $name \
+        $volume_opts \
+        $name:$version
+    
+    if ask "Shell into container?"
+    then
+        docker exec -it $name bash
+    fi
 fi
