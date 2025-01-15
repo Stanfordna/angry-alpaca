2ddf959e1846b936de64ff510fa83b713268c97e
Nicholas <stanfordnas@gmail.com>
2025-01-14 17:05:27 -0700

    20250114
    Added option to publish to remote repo.

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 version='0.1.0'
 name='angry-alpaca'
-docker_hub_url=''
+docker_hub_url='TBD'
 build_args="\
   --build-arg VERSION=$version \
   --build-arg CREATE=$(date +%Y-%m-%dT%H:%M:%SZ) \
@@ -11,5 +11,9 @@ docker build $docker_hub_url$name:$version
 
 if ask "Publish image to Docker Hub?"
 then
-    docker exec -it $name bash
+    docker push $docker_hub_url$name:$version
+    echo "Uploaded Images:"
+    echo "- $docker_hub_url$name:$version"
+    docker push $docker_hub_url$name:latest
+    echo "- $docker_hub_url$name:latest"
 fi
