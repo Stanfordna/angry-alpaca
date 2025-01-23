2ce207ec01116a06a0776e6e65180309f935f6c9
Nicholas <stanfordnas@gmail.com>
2025-01-23 11:34:11 -0700

    Added docker_build_and_publish.sh non-interactive mode
    You can pass in -y or --non-interactive to avoid having to enter yes or no. Useful for pipeline.

--- scripts/docker_build_and_publish.sh
+++ scripts/docker_build_and_publish.sh
@@ -8,10 +8,25 @@ build_args="\
     --build-arg CREATE=$(date +%Y-%m-%dT%H:%M:%SZ) \
     "
 
+# Use this to avoid y/n prompts and for pipeline
+if [ "$1" = "--non-interactive" ] || [ "$1" = "-y" ]
+then
+    echo "Building Image: $docker_hub_url/$name:$version"
+    docker build .. --tag $docker_hub_url/$name:$version \
+        $build_args \
+        -f ../Dockerfile ..
+    echo "Pushing Image: $docker_hub_url/$name:$version"
+    docker push $docker_hub_url/$name:$version
+    docker tag $docker_hub_url/$name:$version $name:$version 
+    exit
+fi
+
 if ask "Build docker image $name:$version?"
 then
     echo "Building Image: $name:$version"
-    docker build --tag $name:$version .
+    docker build .. --tag $name:$version \
+        $build_args \
+        -f ../Dockerfile
 fi
 
 if ask "Publish image to $name:$version Docker Hub?"
